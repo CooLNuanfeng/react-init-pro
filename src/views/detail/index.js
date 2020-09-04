039f8685d5c8af09687668db7509fc392970b8b3
wuhongsong <wuhongsong@haier.com>
2020-09-04 18:05:32 +0800

    init

--- src/views/detail/index.js
+++ src/views/detail/index.js
@@ -22,10 +22,71 @@ import { Button } from 'antd-mobile';
 
 function useCustom(page){
   useEffect(()=>{
-    http.get('/sg/cms/middleImageConfig.json?page='+page).then(res=>{
-      console.log(res)
-    })
+    console.log('++++++++++++')
   })
+}
+
+
+function PostList({ count = 5 }) {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    let p = document.createElement("p");
+    p.innerHTML = `当前文章数量：${count}`;
+    document.getElementById('page').append(p);
+    return ()=>{
+      p.remove()
+    }
+  });
+
+  useEffect(() => {
+    const loadPosts = async () => {
+      setLoading(true);
+      const response = await fetch(
+        `/posts?_limit=${count}`
+      );
+      const data = await response.json();
+      setPosts(data);
+      setLoading(false);
+    };
+  
+    loadPosts();
+  },[count]);
+
+
+  if (loading) return <div>loading...</div>;
+
+  return (
+    <ul>
+      {posts.slice(0, count).map((post, index) => {
+        return <li key={post.id}>{index+1}、 {post.title}</li>;
+      })}
+    </ul>
+  );
+}
+
+const Info = ()=> {
+  const [showList, setShowList] = useState(false);
+  const [postCount, setPostCount] = useState(5);
+  return (
+    <>
+      <button onClick={() => setShowList(!showList)}>
+        {showList ? "隐藏" : "显示"}
+      </button>
+      <button onClick={() => setPostCount(previousCount => previousCount + 1)}>
+        增加数量
+      </button>
+      {showList && <PostList count={postCount} />}
+    </>
+  )
+}
+
+const DetailTitle = () => {
+  console.log('title')
+  return (
+    <h1>Detail app</h1>
+  )
 }
 
 
@@ -34,7 +95,9 @@ const Detail = ()=>{
   useCustom(page)
   return (
     <>
-      <h1>Detail app</h1>
+      <DetailTitle/>
+      <Info/>
+      <div id="page"></div>
       <Button type="primary" onClick={()=>setPage(page+1)}>click</Button>
     </>
   )
